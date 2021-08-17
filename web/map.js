b23f565f10cacf5bd05cf83418b3f65d83b97d07
tiwo <tiwocode@gmail.com>
2021-08-17 12:06:03 +0200

    .

--- web/map.js
+++ web/map.js
@@ -7,13 +7,23 @@ ba_map.locate({setView: true, maxZoom: 16});
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
+    tileSize: 512,
+    zoomOffset: -1,
+    id: 'mapbox/streets-v11',
+    accessToken: 'pk.eyJ1IjoidGl3byIsImEiOiJja3Nmdnc5a3oxZThoMzBvZHV4eTN0eXhwIn0.5eyeTLtyEYGIm8oCKbEbGg'
+})
+
+/*
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
     // id: 'mapbox/streets-v11',
     id: 'tiwo/ckc70qqij1t3z1iowlwrfti8q',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoidGl3byIsImEiOiJja3Nmdnc5a3oxZThoMzBvZHV4eTN0eXhwIn0.5eyeTLtyEYGIm8oCKbEbGg'
 }).addTo(ba_map);
-
+*/
 
 
 
