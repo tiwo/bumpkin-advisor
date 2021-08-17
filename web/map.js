846388cbd471fa38ea750b93beef2ebb70130778
tiwo <tiwocode@gmail.com>
2021-08-17 11:51:16 +0200

    .

--- web/map.js
+++ web/map.js
@@ -1,6 +1,6 @@
 // (()=>{
 
-var ba_map = L.map('mapid').setView([51.505, -0.09], 13);
+var ba_map = L.map('ba-map').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
