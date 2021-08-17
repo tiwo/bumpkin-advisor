9bbf0cd14b288315ee47f3db3911ac354659f373
tiwo <tiwocode@gmail.com>
2021-08-17 12:24:34 +0200

    .

--- web/map.js
+++ web/map.js
@@ -2,14 +2,14 @@
 
 var ba_map = L.map('ba-map').fitWorld();
 
-ba_map.locate({setView: true, maxZoom: 16});
+ba_map.locate({setView: true, maxZoom: 10});
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     tileSize: 512,
     zoomOffset: -1,
-    id: 'tiwo/ckc70qqij1t3z1iowlwrfti8q',
+    id: 'mapbox/streets-v11',
     accessToken: 'pk.eyJ1IjoidGl3byIsImEiOiJja3Nmdnc5a3oxZThoMzBvZHV4eTN0eXhwIn0.5eyeTLtyEYGIm8oCKbEbGg'
 })
 
